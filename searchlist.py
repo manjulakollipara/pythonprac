85b2164dd4c726c624648dfd4c7b994e32fb8768
Manjula Kollipara <manjusivak@gmail.com>
2021-09-29 02:40:21 -0700

    Add files via upload

---
+++ searchlist.py
@@ -0,0 +1,9 @@
+# searching through the list
+
+student_list1 = ['John','Tim','Ron','Ali','Hero']
+def search_student(student_list):
+    for s in student_list:
+        if s == 'Ali':
+            print("Yes student is available")
+
+search_student(student_list1)
